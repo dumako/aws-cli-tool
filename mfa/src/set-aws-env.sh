7899c268e38b3b4ee6c93d4d020c00c08239c4c6
dumako <asazumarisa@gmail.com>
2022-01-16 21:45:17 +0900

    bug-fix: to overwrite

--- mfa/src/set-aws-env.sh
+++ mfa/src/set-aws-env.sh
@@ -7,7 +7,7 @@ token=$3
 conf_dir=$4
 
 # add the credential to aws credentials
-cat <<EOL >> ${conf_dir}/.mfa-token
+cat <<EOL > ${conf_dir}/.mfa-token
 export AWS_ACCESS_KEY_ID=${acckey}
 export AWS_SECRET_ACCESS_KEY=${seckey}
 export AWS_SESSION_TOKEN=${token}
